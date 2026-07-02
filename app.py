5c8c04f493a0c485c26433361dce29aee9135308
zeeshanhaidar-mangar <zeeshanhaidar.ai@gmail.com>
2026-07-02 12:30:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,9 @@ def predict():
         })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=False,
+        use_reloader=False
+    )
